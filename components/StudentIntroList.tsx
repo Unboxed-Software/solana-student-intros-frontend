7744ce06ad368be4433f943cb4237a536a30166e
James Pacheco <jamesrp13@gmail.com>
2022-04-25 12:24:33 -0600

    Remove deserialization code

--- components/StudentIntroList.tsx
+++ components/StudentIntroList.tsx
@@ -1,26 +1,14 @@
 import { Card } from './Card'
 import { FC, useEffect, useState } from 'react'
 import { StudentIntro } from '../models/StudentIntro'
-import * as web3 from '@solana/web3.js'
 
 const STUDENT_INTRO_PROGRAM_ID = '6wNDDbfhqyY8Nm8H2dzAPywjt2D7VKfBzKuSjE3pcgVr'
 
 export const StudentIntroList: FC = () => {
-    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
     const [studentIntros, setStudentIntros] = useState<StudentIntro[]>([])
 
     useEffect(() => {
-        connection.getProgramAccounts(new web3.PublicKey(STUDENT_INTRO_PROGRAM_ID)).then(async (accounts) => {
-            const studentIntros: StudentIntro[] = accounts.reduce((accum: StudentIntro[], { pubkey, account }) => {
-                const studentIntro = StudentIntro.deserialize(account.data)
-                if (!studentIntro) {
-                    return accum
-                }
-
-                return [...accum, studentIntro]
-            }, [])
-            setStudentIntros(studentIntros)
-        })
+        setStudentIntros(StudentIntro.mocks)
     }, [])
     
     return (
--- models/StudentIntro.ts
+++ models/StudentIntro.ts
@@ -21,29 +21,9 @@ export class StudentIntro {
         borsh.str('message'),
     ])
 
-    static borshAccountSchema = borsh.struct([
-        borsh.bool('initialized'),
-        borsh.str('name'),
-        borsh.str('message'),
-    ])
-
     serialize(): Buffer {
         const buffer = Buffer.alloc(1000)
         this.borshInstructionSchema.encode({ ...this, variant: 0 }, buffer)
         return buffer.slice(0, this.borshInstructionSchema.getSpan(buffer))
-    }
-
-    static deserialize(buffer?: Buffer): StudentIntro|null {
-        if (!buffer) {
-            return null
-        }
-
-        try {
-            const { name, message } = this.borshAccountSchema.decode(buffer)
-            return new StudentIntro(name, message)
-        } catch(e) {
-            console.log('Deserialization error:', e)
-            return null
-        }
     }
 }
